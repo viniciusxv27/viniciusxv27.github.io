eb940f3768bccbe65d21a2af66f97113df982a07
Vinicius Costa <83793571+viniciusxv27@users.noreply.github.com>
2022-05-21 11:54:43 -0300

    Add files via upload

---
+++ pagina4.js
@@ -0,0 +1,7 @@
+function notificacao_do_link() {
+    return window.alert("Link Ainda Será Disponibilizado, Deploy ainda está em Andamento...")
+}
+
+function repositorio() {
+    return window.alert("Não Disponível em Repositorio GITHUB")
+}
