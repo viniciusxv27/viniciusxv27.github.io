34bdfed9fb43c18e50b2a5bfc1ed5df0781db92f
Vinicius Costa <83793571+viniciusxv27@users.noreply.github.com>
2022-05-25 01:14:39 -0300

    Update pagina4.js

--- pagina4.js
+++ pagina4.js
@@ -1,7 +1,3 @@
-function notificacao_do_link() {
-    return window.alert("Link Ainda Será Disponibilizado, Deploy ainda está em Andamento...")
-}
-
 function repositorio() {
     return window.alert("Não Disponível em Repositorio GITHUB")
 }
